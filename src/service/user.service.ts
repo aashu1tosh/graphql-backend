43686bf874a0bde872a51afb2a67e1bb7c6a1098
Aashutosh Parajuli <aashutosh282@gmail.com>
2024-07-01 15:23:56 +0545

    add: login service added

--- src/service/user.service.ts
+++ src/service/user.service.ts
@@ -1,19 +1,41 @@
-import { CreateUserPayload } from "../interface/user.interface"
-import { prismaClient } from "../lib/db"
-import bcryptServiceObj from "./bcrypt.service"
+import { CreateUserPayload, LoginPayload } from '../interface/user.interface';
+import { prismaClient } from '../lib/db';
+import bcryptServiceObj from './bcrypt.service';
 
 export class UserService {
     async createUser(payload: CreateUserPayload) {
-        const {firstName, lastName, email, password}  = payload
-        const hashedPassword =  await bcryptServiceObj.hash(password)
+        const { firstName, lastName, email, password } = payload;
+        const hashedPassword = await bcryptServiceObj.hash(password);
         return prismaClient.user.create({
             data: {
                 firstName,
                 lastName,
                 email,
-                password: hashedPassword
-            }
-        })
-    }   
+                password: hashedPassword,
+            },
+        });
+    }
 
+    async login(payload: LoginPayload) {
+        try {
+            const { email, password } = payload;
+            const user = await prismaClient.user.findUnique({
+                where: {
+                    email: email, // Assuming email is the unique field in your User model
+                },
+            });
+            if (user) {
+                const validCredential = await bcryptServiceObj.compare(
+                    password,
+                    user.password
+                );
+                if (validCredential) return 'true';
+                return 'false';
+            } else {
+                throw new Error('Invalid Credentials');
+            }
+        } catch (error) {
+            throw new Error('Invalid Credentials');
+        }
+    }
 }

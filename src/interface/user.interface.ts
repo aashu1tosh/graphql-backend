80ba1456e37665b73efbf96b61e59c2370baee52
Aashutosh Parajuli <aashutosh282@gmail.com>
2024-07-01 15:23:19 +0545

    add: loginPayload added

--- src/interface/user.interface.ts
+++ src/interface/user.interface.ts
@@ -1,6 +1,11 @@
 export interface CreateUserPayload {
-    firstName: string
-    lastName?: string
-    email: string
-    password: string
+    firstName: string;
+    lastName?: string;
+    email: string;
+    password: string;
+}
+
+export interface LoginPayload {
+    email: string;
+    password: string;
 }

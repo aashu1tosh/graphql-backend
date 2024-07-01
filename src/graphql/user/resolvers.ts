11270cb54dc91b1692c5dd651bd9ba421ac3ff8d
Aashutosh Parajuli <aashutosh282@gmail.com>
2024-07-01 12:41:55 +0545

    wip: user create service done

--- src/graphql/user/resolvers.ts
+++ src/graphql/user/resolvers.ts
@@ -1,8 +1,19 @@
+import { CreateUserPayload } from "../../interface/user.interface";
+import { UserService } from "../../service/user.service";
+
 const queries = {};
 
+const userService = new UserService();
+
 const mutations = {
-    createUser: async (_: any, {}: {}) => {
-        return 'randomId';
+    createUser: async (_: any, payload: CreateUserPayload) => {
+        try {
+            const res = await userService.createUser(payload);
+            return res.id;
+        } catch (error) {
+            console.error('Error creating user:', error);
+            // throw error;
+        }
     },
 };
 

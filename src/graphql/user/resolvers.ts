3070752f05710400175b6491d7a93d2cc1ac522f
Aashutosh Parajuli <aashutosh282@gmail.com>
2024-07-01 15:22:41 +0545

    resolvers login validation

--- src/graphql/user/resolvers.ts
+++ src/graphql/user/resolvers.ts
@@ -1,9 +1,21 @@
-import { CreateUserPayload } from "../../interface/user.interface";
-import { UserService } from "../../service/user.service";
-
-const queries = {};
+import {
+    CreateUserPayload,
+    LoginPayload,
+} from '../../interface/user.interface';
+import { UserService } from '../../service/user.service';
 
 const userService = new UserService();
+
+const queries = {
+    LoginValidation: async (_: any, payload: LoginPayload) => {
+        try {
+            const response = await userService.login(payload);
+            return response;
+        } catch (error) {
+            throw new Error('Invalid Credentials');
+        }
+    },
+};
 
 const mutations = {
     createUser: async (_: any, payload: CreateUserPayload) => {
